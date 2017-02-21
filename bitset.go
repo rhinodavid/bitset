084172dfa709a952a448d98c6f265f9479296740
David Walsh <dawalsh@gmail.com>
2017-02-20 21:09:15 -0500

    Initial commit

---
+++ bitset.go
@@ -0,0 +1,111 @@
+package bitset
+
+import "log"
+
+// Bitset represents a set of positive integers as a single integer. For each integer
+// in the set, the corresponding bit in the resultant Bitset to 1.
+type Bitset int
+
+func pow(base, exp int) (r int) {
+	if exp == 0 {
+		return 1
+	}
+	r = base
+	for i := 1; i < exp; i++ {
+		r *= base
+	}
+	return
+}
+
+// NewFromSlice takes a slice of positive integers and returns the corresponding
+// Bitset
+func NewFromSlice(a []int) (r Bitset) {
+	for i, v := range a {
+		if v < 0 {
+			log.Fatalf("All values in the slice must be positive. Saw %d at index %d", v, i)
+		}
+		r = r + Bitset(pow(2, v))
+	}
+	return
+}
+
+// ToSlice returns an array of integers representing the members of the set
+// contained in the Bitset
+func (b Bitset) ToSlice() []int {
+	x := b
+	r := make([]int, 0)
+	for i := 0; x > 0; i++ {
+		if x&1 == 1 {
+			r = append(r, i)
+		}
+		x = x >> 1
+	}
+	return r
+}
+
+// RemoveMember takes an integer value
+func (b Bitset) RemoveMember(i int) (r Bitset) {
+	if i < 0 {
+		log.Fatalf("Attempt to remove negative member from the Bitset: %d", i)
+	}
+	// check to see if index exists
+	if b&Bitset(pow(2, i)) == Bitset(pow(2, i)) {
+		r = b
+		return r - Bitset(pow(2, i))
+	}
+	return b
+}
+
+// Contains accepts a positive integer and returns true if it is
+// contained in the Bitset and false if it is not
+func (b Bitset) Contains(i int) bool {
+	if i < 0 {
+		log.Fatalf("Attempt to check negative member in Bitset: %d", i)
+	}
+	return b&Bitset(pow(2, i)) == Bitset(pow(2, i))
+}
+
+func countBits(x int) (r int) {
+	for y := x; y > 0; y = y >> 1 {
+		if y&1 == 1 {
+			r++
+		}
+	}
+	return
+}
+
+func applyMask(a []int, m int) []int {
+	r := make([]int, 0)
+	n := len(a)
+	for q := 0; m > 0; q++ {
+		if m&1 == 1 {
+			if q < n {
+				r = append(r, a[q])
+			}
+		}
+		m = m >> 1
+	}
+	return r
+}
+
+// PowerSet takes operates on a Bitset and returns an array
+// where the index is the number of items in the powersets
+// and the values are slices of Bitsets representing those
+// powersets
+func (b Bitset) PowerSet() [][]Bitset {
+	set := b.ToSlice()
+	n := len(set)
+	result := make([][]Bitset, n+1)
+	mask := pow(2, n) - 1
+	for ; mask >= 0; mask-- {
+		subSet := applyMask(set, mask)
+		bs := NewFromSlice(subSet)
+		ssLen := len(subSet)
+		if result[ssLen] == nil {
+			result[ssLen] = []Bitset{bs}
+		} else {
+			result[ssLen] = append(result[ssLen], bs)
+		}
+	}
+	return result
+}
---
+++ bitset_test.go
@@ -0,0 +1,74 @@
+package bitset
+
+import (
+	"reflect"
+	"testing"
+)
+
+var (
+	arrs    = [][]int{[]int{}, []int{0}, []int{1}, []int{0, 1, 2}, []int{1, 4, 10}}
+	bitsets = []Bitset{0, 1, 2, 7, 1042}
+)
+
+func TestCreate(t *testing.T) {
+	for i, arr := range arrs {
+		bs := NewFromSlice(arr)
+		if bs != bitsets[i] {
+			t.Fatalf("Incorrect bitset for %v, expected %d, got %d", arr, bitsets[i], bs)
+		}
+	}
+}
+
+func TestToSlice(t *testing.T) {
+	for i, bs := range bitsets {
+		arr := bs.ToSlice()
+		if !reflect.DeepEqual(arr, arrs[i]) {
+			t.Fatalf("Incorrect slice for bitset %d, expected %v, got %v", bs, arrs[i], arr)
+		}
+	}
+}
+
+func TestRemove(t *testing.T) {
+	bs := NewFromSlice([]int{1, 4, 10})
+	result := bs.RemoveMember(10)
+	if !reflect.DeepEqual(result.ToSlice(), []int{1, 4}) {
+		t.Fatalf("Incorrect result removing index. Got %d", result)
+	}
+}
+
+func TestContains(t *testing.T) {
+	bs := NewFromSlice([]int{0, 2, 4, 5, 7, 24})
+	testMembers := []int{0, 1, 2, 15, 24, 25}
+	answers := []bool{true, false, true, false, true, false}
+	for i, v := range testMembers {
+		if result := bs.Contains(v); result != answers[i] {
+			t.Fatalf("Incorrect result with member %d, expected %v, got %v", v, answers[i], result)
+		}
+	}
+}
+
+func TestApplyMask(t *testing.T) {
+	arr := []int{1, 2, 3}
+	masks := []int{0, 1, 2, 3}
+	answers := [][]int{[]int{}, []int{1}, []int{2}, []int{1, 2}}
+	for i, mask := range masks {
+		if result := applyMask(arr, mask); !reflect.DeepEqual(result, answers[i]) {
+			t.Fatalf("Incorrect result from apply mask. Applying %d, expected %v, got %v", mask, answers[i], result)
+		}
+	}
+}
+
+func TestPowerSet(t *testing.T) {
+	arr := []int{1, 2, 3}
+	bs := NewFromSlice(arr)
+	ps := bs.PowerSet()
+	if len(ps) != len(arr)+1 {
+		t.Fatalf("Incorrect length of powerset")
+	}
+	if ps[0][0] != NewFromSlice([]int{}) {
+		t.Fatalf("Empty set not present in powersets of length 0")
+	}
+	if ps[len(arr)][0] != NewFromSlice([]int{1, 2, 3}) {
+		t.Fatalf("Full set not present in powersets of length n")
+	}
+}
